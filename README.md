c53bd3a08233f9ba85c2ee6ba8dffc50aa3271d6
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-07-20 16:06:02 -0500

    graphs done, most picked player and captain done

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 
+Things to keep in mind:   
+This will need to be adjusted for seasons past 19/20   
+Also, need to adjust for the final gameweek (should just be a numerical adjustment somewhere)   
+Can break sections into functions later, but for now this is good as an MVP   
+Optimize this, this can be way quicker
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Line} from 'react-chartjs-2';
 
 export default class Home extends React.Component {
 
@@ -10,8 +11,39 @@ export default class Home extends React.Component {
 			overall_rank: 0,
 			best_overall_rank: 0,
 			best_overall_rank_gw: 0,
+			// do we need the following two?
 			pointshistory: [],
-			rankhistory: []
+			rankhistory: [],
+			pointschart: {
+				labels: [],
+				datasets: [
+					{
+						label: 'Points',
+						fill: false,
+						lineTension: 0,
+						backgroundColor: 'rgba(75,192,192,1)',
+						borderColor: 'rgba(0,0,0,1)',
+						borderWidth: 2,
+						data: []
+					}
+				]
+			},
+			rankchart: {
+				labels: [],
+				datasets: [
+					{
+						label: 'Rank',
+						fill: false,
+						lineTension: 0,
+						backgroundColor: 'rgba(75,192,192,1)',
+						borderColor: 'rgba(0,0,0,1)',
+						borderWidth: 2,
+						data: []
+					}
+				]
+			},
+			mostpickedplayer: '',
+			mostcaptainedplayer: ''
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -21,7 +53,26 @@ export default class Home extends React.Component {
     	this.setState({id: event.target.value});
 	}
 
-// fetching data, fix CORS without the browser hacky approach
+	mostFrequentElement(arr) {
+		var mf = 1;
+		var m = 0;
+		var item;
+
+		for (var i = 0; i < arr.length; i++) {
+			for (var j = i; j < arr.length; j++) {
+				if (arr[i] === arr[j]) m++;
+				if (mf < m) {
+					mf = m;
+					item = arr[i];
+				}
+  			}
+  			m = 0;
+		}
+		
+		return item
+	}
+
+	// fetching data, fix CORS without the browser hacky approach
 	getData = async() => {
 
 		// getting the general data
@@ -52,19 +103,65 @@ export default class Home extends React.Component {
 
 		this.setState({best_overall_rank: bestrank, best_overall_rank_gw: bestrankgameweek})
 
-		// getting points and rank data from previous seasons
+		// getting points and rank data from previous and current seasons for the graphs
 		var pointshistory = []
 		var rankhistory = []
 
 		for (var j = 0; j < jsonResponse2.past.length; j++){
 			pointshistory.push(jsonResponse2.past[j].total_points)
 			rankhistory.push(jsonResponse2.past[j].rank)
+			this.state.pointschart.labels.push(jsonResponse2.past[j].season_name)
+			this.state.rankchart.labels.push(jsonResponse2.past[j].season_name)
 		}
 
 		pointshistory.push(this.state.total_points)
 		rankhistory.push(this.state.overall_rank)
+		this.state.pointschart.labels.push("2019/20")
+		this.state.rankchart.labels.push("2019/20")
 
+		this.state.pointschart.datasets[0].data = pointshistory
+		this.state.rankchart.datasets[0].data = rankhistory
+
+		// do we need these as well?
 		this.setState({pointshistory: pointshistory, rankhistory: rankhistory})
+
+		// player and captain picks now
+		var picks = []
+		var captains = []
+
+		for (var k = 1; k < 47; k++){
+			if (k < 30 || k > 38){
+				const response3 = await fetch('https://fantasy.premierleague.com/api/entry/' + this.state.id + '/event/' + k + '/picks/', 
+					{method: 'GET'})
+				const jsonResponse3 = await response3.json()
+
+				for (var l = 0; l < jsonResponse3.picks.length; l++){
+					if (jsonResponse3.picks[l].multiplier != 0){
+						picks.push(jsonResponse3.picks[l].element)
+					}
+
+					if (jsonResponse3.picks[l].is_captain == true){
+						captains.push(jsonResponse3.picks[l].element)
+					}
+				}
+			}
+		}
+
+		var mfcaptainid = this.mostFrequentElement(captains)
+		var mfpickid = this.mostFrequentElement(picks)
+
+		const response4 = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/', {method: 'GET'})
+		const jsonResponse4 = await response4.json()
+
+		for (var m = 0; m < jsonResponse4.elements.length; m++){
+			if (mfcaptainid === jsonResponse4.elements[m].id){
+				this.setState({mostcaptainedplayer: jsonResponse4.elements[m].web_name})
+			}
+
+			if (mfpickid === jsonResponse4.elements[m].id){
+				this.setState({mostpickedplayer: jsonResponse4.elements[m].web_name})
+			}
+		}
 	}
 
 	render() {
@@ -89,7 +186,30 @@ export default class Home extends React.Component {
 					Total points: {this.state.total_points}<br/>
 					Overall Rank: {this.state.overall_rank}<br/>
 					Best Overall Rank: {this.state.best_overall_rank}, obtained in gameweek {this.state.best_overall_rank_gw} <br />
-
+			        <Line data={this.state.pointschart}
+						options={{
+							title:{
+								display:true,
+								text:'Points History',
+								fontSize:20
+							},
+							legend: {
+							display: false						
+							}
+					}} />
+			        <Line data={this.state.rankchart}
+						options={{
+							title:{
+								display: true,
+								text: 'Rank History',
+								fontSize: 20
+							},
+							legend: {
+							display: false						
+							}
+					}}/>
+					The player with the most appearances was: {this.state.mostpickedplayer}<br />
+					Most frequently captained: {this.state.mostcaptainedplayer}<br />
 				</div>
 			);
 		}
