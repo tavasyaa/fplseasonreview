ebd0848c4abfed42c7fe076098128618d3747af0
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-07-22 13:07:46 -0500

    fixed missed gameweek and undefined pie issue, slightly cleaner code too

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This will need to be adjusted for seasons past 19/20
 Also, need to adjust for the final gameweek (should just be a numerical adjustment somewhere)   
 Can break sections into functions later, but for now this is good as an MVP   
 Optimize this, this can be way quicker   
-Some variables don't need to be stored on the client before updating state
+Some variables don't need to be stored on the client before updating state   
+Make loading prioritized so the things that take less time are displayed first, keep in mind that the piechart will break if you update before the variables are ready
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Home.js
+++ src/components/Home.js
@@ -49,7 +49,7 @@ export default class Home extends React.Component {
 			freehitgw: 0,
 			triplecapgw: 0,
 			wildcardstats: [],
-			piechart: {
+			doughnut: {
 				labels: ['GK', 'DEF', 'MID', 'ST'],
 				datasets: [
 					{
@@ -122,47 +122,44 @@ export default class Home extends React.Component {
 		const jsonResponse1 = await response1.json()
 
 		// getting the history data
-		const response2 = await fetch('https://fantasy.premierleague.com/api/entry/' + this.state.id + '/history/', {method:'GET'})
-		const jsonResponse2 = await response2.json()
+		const historydata = await fetch('https://fantasy.premierleague.com/api/entry/' + this.state.id + '/history/', {method:'GET'})
+		const jsonhistorydata = await historydata.json()
 
 		// bootstrap static, this is the big store of info
-		const response4 = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/', {method: 'GET'})
-		const jsonResponse4 = await response4.json()
+		const bootstrapdata = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/', {method: 'GET'})
+		const jsonbootstrapdata = await bootstrapdata.json()
 
 		this.setState({total_points: jsonResponse1.entry_history.total_points, overall_rank: jsonResponse1.entry_history.overall_rank})
 
 		// getting best overall rank
-		var bestrank = 10000000
-		var bestrankgameweek = 0
+		var bestrank = 10000000, bestrankgameweek = 0
 		// getting highest points for a single gameweek
-		var bestpoints = 0
-		var bestpointsgameweek = 0
+		var bestpoints = 0, bestpointsgameweek = 0
 		// getting highest gameweek rank
-		var bestgwrank = 100000000
-		var bestgwrankgameweek = 0
+		var bestgwrank = 100000000, bestgwrankgameweek = 0
 
 
-		for (var i = 0; i < jsonResponse2.current.length; i++){
+		for (var i = 0; i < jsonhistorydata.current.length; i++){
 
-			if(jsonResponse2.current[i].overall_rank < bestrank){
-				bestrank = jsonResponse2.current[i].overall_rank
+			if(jsonhistorydata.current[i].overall_rank < bestrank){
+				bestrank = jsonhistorydata.current[i].overall_rank
 				bestrankgameweek = i+1
 				if (bestrankgameweek > 30){
 					bestrankgameweek = bestrankgameweek - 9
 				}
 			}
 
-			if(jsonResponse2.current[i].points > bestpoints){
-				bestpoints = jsonResponse2.current[i].points
+			if(jsonhistorydata.current[i].points > bestpoints){
+				bestpoints = jsonhistorydata.current[i].points
 				bestpointsgameweek = i+1
 				if (bestpointsgameweek > 30){
 					bestpointsgameweek = bestpointsgameweek - 9
 				}
 			}
 			
-			if(i < 29 || i > 37){
-				if(jsonResponse2.current[i].rank < bestgwrank){
-					bestgwrank = jsonResponse2.current[i].rank
+			if(i < 30 || i > 38){
+				if(jsonhistorydata.current[i].rank < bestgwrank){
+					bestgwrank = jsonhistorydata.current[i].rank
 					bestgwrankgameweek = i+1
 					if (bestgwrankgameweek > 30){
 						bestgwrankgameweek = bestgwrankgameweek - 9
@@ -178,11 +175,11 @@ export default class Home extends React.Component {
 		var pointshistory = []
 		var rankhistory = []
 
-		for (var j = 0; j < jsonResponse2.past.length; j++){
-			pointshistory.push(jsonResponse2.past[j].total_points)
-			rankhistory.push(jsonResponse2.past[j].rank)
-			this.state.pointschart.labels.push(jsonResponse2.past[j].season_name)
-			this.state.rankchart.labels.push(jsonResponse2.past[j].season_name)
+		for (var j = 0; j < jsonhistorydata.past.length; j++){
+			pointshistory.push(jsonhistorydata.past[j].total_points)
+			rankhistory.push(jsonhistorydata.past[j].rank)
+			this.state.pointschart.labels.push(jsonhistorydata.past[j].season_name)
+			this.state.rankchart.labels.push(jsonhistorydata.past[j].season_name)
 		}
 
 		pointshistory.push(this.state.total_points)
@@ -205,9 +202,11 @@ export default class Home extends React.Component {
 
 
 		// this loop sucks, draw this out and see if you can figure a way better than a triple loop :(
-		// how the fuck are we missing 44 points?????
+		// earlier we had a problem with k so we missed gw 29 but that's fixed now. disparity between total points and this
+		// is because of deducted points for extra transfers
+		// we still have a one point discrepancy, but i think that's because of update lag, check later
 		for (var k = 1; k < 47; k++){
-			if (k < 29 || k > 37){
+			if (k < 30 || k > 38){
 				const response3 = await fetch('https://fantasy.premierleague.com/api/entry/' + this.state.id + '/event/' + k + '/picks/', 
 					{method: 'GET'})
 				const jsonResponse3 = await response3.json()
@@ -224,13 +223,13 @@ export default class Home extends React.Component {
 
 						// updating points, but this takes way too long
 						points.push(jsonResponse5.elements.find(o => o.id === jsonResponse3.picks[l].element).stats.total_points*jsonResponse3.picks[l].multiplier)
-						positions.push(jsonResponse4.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type)
+						positions.push(jsonbootstrapdata.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type)
 
-						if (jsonResponse4.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 1){
+						if (jsonbootstrapdata.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 1){
 							gkpoints = gkpoints + jsonResponse5.elements.find(o => o.id === jsonResponse3.picks[l].element).stats.total_points*jsonResponse3.picks[l].multiplier
-						} else if(jsonResponse4.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 2) {
+						} else if(jsonbootstrapdata.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 2) {
 							defpoints = defpoints + jsonResponse5.elements.find(o => o.id === jsonResponse3.picks[l].element).stats.total_points*jsonResponse3.picks[l].multiplier
-						} else if(jsonResponse4.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 3) {
+						} else if(jsonbootstrapdata.elements.find(o => o.id === jsonResponse3.picks[l].element).element_type === 3) {
 							midpoints = midpoints + jsonResponse5.elements.find(o => o.id === jsonResponse3.picks[l].element).stats.total_points*jsonResponse3.picks[l].multiplier
 						} else {
 							fwdpoints = fwdpoints + jsonResponse5.elements.find(o => o.id === jsonResponse3.picks[l].element).stats.total_points*jsonResponse3.picks[l].multiplier
@@ -245,18 +244,8 @@ export default class Home extends React.Component {
 			}
 		}
 
-		console.log(gkpoints, defpoints, midpoints, fwdpoints)
-		var sum = 0;
-		for(var s = 0; s < points.length; s++){
-			sum = points[s] + sum;
-		}
-		console.log(sum)
-		console.log(points)
-		console.log(gameweeks)
-		this.state.piechart.datasets[0].data.push(gkpoints)
-		this.state.piechart.datasets[0].data.push(defpoints)
-		this.state.piechart.datasets[0].data.push(midpoints)
-		this.state.piechart.datasets[0].data.push(fwdpoints)
+		// updating the donut
+		this.state.doughnut.datasets[0].data.push(gkpoints, defpoints, midpoints, fwdpoints)
 
 		// these are still in ID form, a number
 		var mfcaptainid = this.mostFrequentElement(captains)
@@ -273,25 +262,25 @@ export default class Home extends React.Component {
 
 
 		// getting the names from the IDs
-		for (var m = 0; m < jsonResponse4.elements.length; m++){
-			if (mfcaptainid === jsonResponse4.elements[m].id){
-				this.setState({mostcaptainedplayer: jsonResponse4.elements[m].web_name})
+		for (var m = 0; m < jsonbootstrapdata.elements.length; m++){
+			if (mfcaptainid === jsonbootstrapdata.elements[m].id){
+				this.setState({mostcaptainedplayer: jsonbootstrapdata.elements[m].web_name})
 			}
 
-			if (mfpickid === jsonResponse4.elements[m].id){
-				this.setState({mostpickedplayer: jsonResponse4.elements[m].web_name})
+			if (mfpickid === jsonbootstrapdata.elements[m].id){
+				this.setState({mostpickedplayer: jsonbootstrapdata.elements[m].web_name})
 			}
 
-			if (maxpointsplayerid === jsonResponse4.elements[m].id){
-				this.setState({maxplayerpointsname: jsonResponse4.elements[m].web_name})
+			if (maxpointsplayerid === jsonbootstrapdata.elements[m].id){
+				this.setState({maxplayerpointsname: jsonbootstrapdata.elements[m].web_name})
 			}
 		}
 	}
 
 	render() {
-		if (this.state.total_points === 0){
+		if (this.state.doughnut.datasets[0].data.length === 0){
 			return(
-				<div>
+				<div className="inputsection">
 					<form>
 						<label>
 							Team ID:
@@ -338,8 +327,9 @@ export default class Home extends React.Component {
 					<h3> Player picks </h3>
 					The player with the most appearances was: {this.state.mostpickedplayer}, with {this.state.mostpickedtimes} appearances <br />
 					Most frequently captained: {this.state.mostcaptainedplayer}, {this.state.captainedtimes} times <br />
-					Maximum points by one player: {this.state.maxplayerpoints}, by {this.state.maxplayerpointsname} in gameweek {this.state.maxplayerpointsgw}
-					<Doughnut data={this.state.piechart} />
+					Maximum points by one player: {this.state.maxplayerpoints}, by {this.state.maxplayerpointsname} in gameweek {this.state.maxplayerpointsgw} <br />
+					<h3> Points by Position</h3>
+					<Doughnut data={this.state.doughnut} />
 
 					<h3> Chips </h3>
 					You used your wildcard in gameweek , and got points, more than average <br />
